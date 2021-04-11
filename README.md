db7fc74c087cd5a14580ba0a101af266b9051029
seanyoung247 <seanyoung247@gmail.com>
2021-04-11 01:12:43 +0100

    docs: initial README.md layout

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # TWCoulsdon
+
+TWCoulsdon is a website for the Theatre Workshop Coulsdon amateur dramatics group.
+
+
+
+## Table of Contents
+
+<TBC>
+
+## User Experience
+
+### Project Goals
+
+### User Stories
+
+## Design
+
+### Database
+
+### Fonts
+
+### Colours
+
+![pallet](docs/images/pallet/pallet.png)
+
+### Layout
+
+## Features
+
+### Existing Features
+
+### Future Features
+
+## Technologies
+
+### Site architecture
+
+### Languages
+
+### Libraries
+
+### Editors
+
+### Tools
+
+### Platforms
+
+## Testing
+
+## Source Control
+
+## Deployment
+
+## Credits
+
--- TWCoulsdon/settings.py
+++ TWCoulsdon/settings.py
@@ -73,13 +73,17 @@ WSGI_APPLICATION = 'TWCoulsdon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -116,8 +120,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
