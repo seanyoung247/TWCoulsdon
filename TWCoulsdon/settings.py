d56100fd0482b33dd38df9ef329acff4ab026561
seanyoung247 <seanyoung247@gmail.com>
2021-04-18 12:50:33 +0100

    feat: adds home app and landing page view

--- TWCoulsdon/settings.py
+++ TWCoulsdon/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -44,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -61,7 +65,9 @@ ROOT_URLCONF = 'TWCoulsdon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
