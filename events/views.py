f4ba6bdc3edf43d191904221c8b326a44f0807a5
seanyoung247 <seanyoung247@gmail.com>
2021-04-24 00:22:49 +0100

    feat: adds events app

---
+++ events/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+def list_events(request):
+    """ A view to show all events, and allows sorting and searching of queries """
+    pass
--- home/views.py
+++ home/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 def index(request):
     """ A view to return the index page """
-
+    #TODO: Get carousel data here
     return render(request, 'home/index.html')
