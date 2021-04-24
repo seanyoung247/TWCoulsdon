5e31f068b17b34ac78e2908f97ac880708bd87e0
seanyoung247 <seanyoung247@gmail.com>
2021-04-24 15:35:42 +0100

    feat: completes models for events app

--- events/models.py
+++ events/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-from datetime import datetime 
+
+from datetime import datetime
+from tinymce.models import HTMLField
+from django_countries.fields import CountryField
 
 
 class ShowType(models.Model):
@@ -16,7 +19,47 @@ class ShowType(models.Model):
 class EventDate(models.Model):
     event = models.ForeignKey('Event', on_delete=models.CASCADE)
     date = models.DateTimeField()
+    
+    def __str__(self):
+        return date.strftime('%d/%m/%Y, %H:%M')
+
+
+class Venue(models.Model):
+    name = models.CharField(max_length=128)
+    image = models.ImageField(null=True, blank=True)
+    description = models.CharField(max_length=1024, null=True, blank=True)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
+    
+    def __str__(self):
+        return self.name
         
     
 class Event(models.Model):
-    pass
+    title = models.CharField(max_length=128)
+    author = models.CharField(max_length=128)
+    tagline = models.CharField(max_length=256)
+    description = HTMLField()
+    type = models.ForeignKey('ShowType', null=True, blank=True, on_delete=models.SET_NULL)
+    venue = models.ForeignKey('Venue', null=True, blank=True, on_delete=models.SET_NULL)
+    title_image = models.ForeignKey('Image', related_name='+', null=True, blank=True, on_delete=models.SET_NULL)
+    
+    def __str__(self):
+        return self.name
+        
+    
+class Image(models.Model):
+    event = models.ForeignKey('Event', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=256, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+    
--- events/views.py
+++ events/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Event list page view 
 def list_events(request):
     """ A view to show all events, and allows sorting and searching of queries """
     return render(request, 'events/events.html')
+    
+
+# Event page view
+
+# Venue page view 
