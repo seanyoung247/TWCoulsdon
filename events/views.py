ca85e681815dd29fc3e4170f83abe9dd5c5c3c63
seanyoung247 <seanyoung247@gmail.com>
2021-04-25 21:35:11 +0100

    feat (US101): adds initial event listing

--- events/views.py
+++ events/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
+from .models import Event, ShowType
 
 # Event list page view 
 def list_events(request):
     """ A view to show all events, and allows sorting and searching of queries """
-    return render(request, 'events/events.html')
+
+    types = None
+    events = None
+    
+    if request.GET:
+        if 'type' in request.GET:
+            types = request.GET['type'].split(',')
+            events = Event.objects.filter(type__name__in=types)
+    
+    
+    context = {
+        'events': events,
+    }
+    
+    return render(request, 'events/events.html', context )
     
 
 # Event page view
