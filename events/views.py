c236a11c0438516554b2b0473aa7ea085c6646df
seanyoung247 <seanyoung247@gmail.com>
2021-04-25 22:58:35 +0100

    feat: (US101) adds sorting of events based on date

--- events/views.py
+++ events/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from .models import Event, ShowType
+from django.db import models
+from django.db.models import Min, Max
+from datetime import datetime
+from .models import Event, ShowType, EventDate
 
 # Event list page view 
 def list_events(request):
@@ -7,12 +10,28 @@ def list_events(request):
 
     types = None
     events = None
+    now = datetime.now()
     
     if request.GET:
         if 'type' in request.GET:
             types = request.GET['type'].split(',')
-            events = Event.objects.filter(type__name__in=types)
-    
+            events = (
+                Event.objects.annotate(
+                    # Is this event in the future or past?
+                    current=models.Case(
+                        models.When(eventdate__date__gte=now, then=True),
+                        models.When(eventdate__date__lt=now, then=False),
+                        output_field=models.BooleanField(),
+                    ),
+                    # Get the first event date for sorting
+                    first_date=Min('eventdate__date')
+                ).filter(
+                    # Filter on required show type
+                    type__name__in=types
+                # Order by inital date in decending order (latest to earliest)
+                ).order_by('-first_date')
+            )
+
     
     context = {
         'events': events,
