4ae2b83f1923a0dbf2e63d0e8543c472f3ba0696
seanyoung247 <seanyoung247@gmail.com>
2021-04-30 20:53:43 +0100

    feat: adds thumbnail creation to image upload

--- TWCoulsdon/settings.py
+++ TWCoulsdon/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django_thumbs',
     
     'allauth',
     'allauth.account',
--- events/models.py
+++ events/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from datetime import datetime
 from tinymce.models import HTMLField
 from django_countries.fields import CountryField
-
+from django_thumbs.fields import ImageThumbsField
 from django.utils.text import slugify
 import itertools
 
@@ -82,10 +82,15 @@ class Event(models.Model):
         
     
 class Image(models.Model):
+    
+    SIZES=(
+        {'code': 'thumb', 'wxh': '125x125', 'resize': 'crop'},
+    )
+    
     event = models.ForeignKey('Event', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=256, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image = ImageThumbsField(null=True, blank=True, sizes=SIZES)
     
     def __str__(self):
         return self.name
