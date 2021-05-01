4222d1ca0f07295a9d17a3c87a3620e98d12ba78
seanyoung247 <seanyoung247@gmail.com>
2021-05-01 14:34:59 +0100

    chore: improves code layout in event.models

--- events/models.py
+++ events/models.py
@@ -1,14 +1,15 @@
+""" Defines database models for the Events app """
+import itertools
 from django.db import models
-from datetime import datetime
-from tinymce.models import HTMLField
+from django.utils.text import slugify
 from django_countries.fields import CountryField
+#from datetime import datetime
+from tinymce.models import HTMLField
 from easy_thumbnails.fields import ThumbnailerImageField
 from embed_video.fields import EmbedVideoField
-from django.utils.text import slugify
-import itertools
-
 
 class ShowType(models.Model):
+    """ Defines the types of Events """
     name = models.CharField(max_length=50)
     display_name = models.CharField(max_length=50, null=True, blank=True)
 
@@ -16,19 +17,22 @@ class ShowType(models.Model):
         return self.name
 
     def get_display_name(self):
+        """ Returns the ShowType's display name """
         return self.display_name
-    
-        
+
+
 class EventDate(models.Model):
+    """ Defines an individual Event performance date """
     event = models.ForeignKey('Event', on_delete=models.CASCADE)
     date = models.DateTimeField()
     #TODO: Add Capacity
-    
+
     def __str__(self):
         return self.date.strftime('%d/%m/%Y, %H:%M')
 
 
 class Venue(models.Model):
+    """ Provides information on a Venue used by Events """
     name = models.CharField(max_length=50)
     image = models.ImageField(null=True, blank=True)
     description = models.CharField(max_length=1024, null=True, blank=True)
@@ -40,12 +44,13 @@ class Venue(models.Model):
     county = models.CharField(max_length=80, null=True, blank=True)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     country = CountryField(blank_label='Country *', null=True, blank=True)
-    
+
     def __str__(self):
         return self.name
-        
-    
+
+
 class Event(models.Model):
+    """ Defines the information and relationships for events """
     slug = models.SlugField(max_length=64, editable=False, null=False, blank=False, unique=True)
     title = models.CharField(max_length=50, null=False, blank=False)
     author = models.CharField(max_length=64, null=True, blank=True)
@@ -53,17 +58,19 @@ class Event(models.Model):
     description = HTMLField()
     type = models.ForeignKey('ShowType', null=True, blank=True, on_delete=models.SET_NULL)
     venue = models.ForeignKey('Venue', null=True, blank=True, on_delete=models.SET_NULL)
-    title_image = models.ForeignKey('Image', related_name='+', null=True, blank=True, on_delete=models.SET_NULL)
+    title_image = models.ForeignKey('Image', related_name='+',
+        null=True, blank=True, on_delete=models.SET_NULL
+    )
     content = EmbedVideoField(null=True, blank=True)
     post_date = models.DateTimeField(auto_now_add=True)
 
     def __generate_slug(self):
         """ Generates a URL slug from the event title """
-        # Based on code from: 
+        # Based on code from:
         # https://simpleit.rocks/python/django/generating-slugs-automatically-in-django-easy-solid-approaches/
-        
+
         # Generate a candidate for the slug
-        max_length = self._meta.get_field('slug').max_length
+        #max_length = self._meta.get_field('slug').max_length
         value = self.title
         slug_candidate = slug_original = slugify(value, allow_unicode=True)
         # Ensure that the candidate is unique
@@ -73,24 +80,24 @@ class Event(models.Model):
             slug_candidate = '{}-{}'.format(slug_original, i)
 
         self.slug = slug_candidate
-    
+
     def __str__(self):
         return self.title
-        
+
     def save(self, *args, **kwargs):
         # Do we need to generate a slug?
         if not self.pk:
             self.__generate_slug()
-        
+
         super().save(*args, **kwargs)
-    
-    
-class Image(models.Model):    
+
+
+class Image(models.Model):
+    """ Stores information on uploaded images """
     event = models.ForeignKey('Event', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=256, null=True, blank=True)
     image = ThumbnailerImageField(null=True, blank=True)
-    
+
     def __str__(self):
         return self.name
-    
