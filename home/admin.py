381a9dba345fd7aa34b7bff0d0bc6de9d59c3ba1
seanyoung247 <seanyoung247@gmail.com>
2021-05-04 23:12:34 +0100

    feat: registers home app models with admin

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Category, Page
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    """ Registers Category model with Django admin """
+    list_display = (
+        'name',
+        'display_name',
+        'title_page',
+    )
+
+
+class PageAdmin(admin.ModelAdmin):
+    """ Registers Page model with Django admin """
+    list_display = (
+        'category',
+        'title',
+        'description',
+        'content',
+        'image',
+    )
+
+    ordering = ('event',)
