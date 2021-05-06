47e752aadb79c3c8c81cb60b8a25027394ab50fd
seanyoung247 <seanyoung247@gmail.com>
2021-05-06 22:28:01 +0100

    feat: adds category and page routes to home app

--- events/urls.py
+++ events/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.list_events, name='events'),
     path('lazy_load/', views.lazy_load_events, name='lazy_load_events'),
     path('venue/<venue_id>', views.venue_details, name='venue_details'),
-    path('<event_slug>', views.event_details, name='event_details'),
+    path('<event_slug>/', views.event_details, name='event_details'),
 ]
--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('<category_slug>/', views.category, name='category'),
+    path('<page_slug>/', views.page, name='page'),
 ]
