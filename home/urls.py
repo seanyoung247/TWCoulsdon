d7e058b296329f803e15a04bb85b3c164f211f60
seanyoung247 <seanyoung247@gmail.com>
2021-05-06 22:47:48 +0100

    fix: fixes url route overriding in home app

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('<category_slug>/', views.category, name='category'),
-    path('<page_slug>/', views.page, name='page'),
+    path('twc/<category_slug>/', views.category, name='category'),
+    path('twc/<page_slug>/', views.page, name='page'),
 ]
