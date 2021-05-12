280b26a4416720bf33c5d92727ef0ee89cb4eec8
seanyoung247 <seanyoung247@gmail.com>
2021-05-12 22:08:47 +0100

    feat: adds models to boxoffice app

--- boxoffice/models.py
+++ boxoffice/models.py
@@ -1,3 +1,94 @@
+import uuid
 from django.db import models
+from django_countries.fields import CountryField
+from events.models import Event, EventDate
+from profiles.models import UserProfile
 
-# Create your models here.
+
+class TicketType(models.Model):
+    """
+    Defines the tickets types (adult, concession)
+    """
+    name = models.CharField(max_length=50, null=False, blank=False)
+    display_name = models.CharField(max_length=50, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Ticket(models.Model):
+    """
+    Defines a single ticket
+    """
+    ticket_id = models.CharField(max_length=32, null=False, blank=False, editable=False)
+    order = models.ForeignKey('Order', null=False, blank=False,
+                                on_delete=models.CASCADE, related_name="tickets")
+    type = models.ForeignKey('TicketType', null=False, blank=False, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, null=False, blank=False, on_delete=models.CASCADE)
+    data = models.ForeignKey(EventDate, null=False, blank=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.ticket_id;
+
+    def _generate_ticket_number(self):
+        return uuid.uuid4().hex.upper()
+
+    def save(self):
+        # Do we need to generate a unique id?
+        if not self.ticket_id:
+            self.ticket_id = self._generate_ticket_number()
+
+        super().save(*args, **kwargs)
+
+
+class Order(models.Model):
+    """
+    Stores information on individual orders
+    """
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
+    date = models.DateTimeField(auto_now_add=True)
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
+
+    def __str__(self):
+        return self.order_number
+
+    def _generate_order_number(self):
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """
+        Recieves the on save/delete signals when related tickets are saved or deleted
+        and updates the order total.
+        """
+
+    def save(self):
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+
+        super().save(*args, **kwargs)
+
+
+
+
+
+
+
+
+
+
+
