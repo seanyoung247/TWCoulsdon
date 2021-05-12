2b16f301542cfb49b10cc8b1ce4e15b406d23f1d
seanyoung247 <seanyoung247@gmail.com>
2021-05-12 22:07:35 +0100

    chore: readibility improvements for profiles apps

--- profiles/views.py
+++ profiles/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 
 @login_required
 def profile(request):
+    """ A View to show a logged in user their own profile """
     return HttpResponse(f'<h1>{request.user.username}</h1>')
