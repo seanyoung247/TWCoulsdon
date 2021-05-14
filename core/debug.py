e02f79f22c39fbb79e2de20d9d2049702621c7bb
seanyoung247 <seanyoung247@gmail.com>
2021-05-14 19:09:05 +0100

    chore: changes debug_print to function

--- core/debug.py
+++ core/debug.py
@@ -1,4 +1,6 @@
 """ Defines helper functions for debugging """
 from django.conf import settings
 
-debug_print = lambda msg: print(msg) if settings.DEBUG else None
+def debug_print(msg):
+    """ Prints to the console only if debugging """
+    if settings.DEBUG: print(msg)
