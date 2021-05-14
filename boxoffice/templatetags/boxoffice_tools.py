4d8b4f28c84df7460e34d871ad52cfcec7f45a95
seanyoung247 <seanyoung247@gmail.com>
2021-05-15 00:33:28 +0100

    feat: adds event date checking to boxoffic templatetags

--- boxoffice/templatetags/boxoffice_tools.py
+++ boxoffice/templatetags/boxoffice_tools.py
@@ -3,18 +3,23 @@
 from django import template
 from django.utils import timezone
 
-from boxoffice.queries import get_available_tickets_for_event
+from events.models import Event, EventDate
+from boxoffice.queries import get_available_tickets_for_event, get_available_tickets_for_date
 
 register = template.Library()
 
 
-@register.filter(name='has_tickets')
-def has_tickets(event):
-    """ Returns true if event still has tickets """
-    return (get_available_tickets_for_event(event) > 0)
-
-
 @register.filter(name='ticket_count')
 def ticket_count(event):
-    """ Returns the number of tickets still available for an event """
-    return get_available_tickets_for_event(event)
+    """ Returns the number of tickets still available for an event or date """
+    if isinstance(event, Event):
+        return get_available_tickets_for_event(event)
+    elif isinstance(event, EventDate):
+        return get_available_tickets_for_date(event)
+    return 0
+
+
+@register.filter(name='has_tickets')
+def has_tickets(event):
+    """ Returns true if event or date still has tickets """
+    return (ticket_count(event) > 0)
