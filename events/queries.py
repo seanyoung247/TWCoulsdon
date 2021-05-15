cbbaf560e7bc196cdef485bfe2cc51f949ac49ca
seanyoung247 <seanyoung247@gmail.com>
2021-05-15 16:19:44 +0100

    feat: atomises event_queries

--- events/queries.py
+++ events/queries.py
@@ -14,6 +14,93 @@ from .models import Event, ShowType
 
 #TODO: Split query events into sub functions
 
+def query_events_by_first_date(event_set, date):
+    """ Filters the set passed on earliest event date
+
+    Parameters:
+    event_set (query_set): A query set of event objects to filter
+    date (string): date string in ISO8601 format
+
+    Returns:
+    (query_set): The filtered event set
+    """
+
+    query = timezone.make_aware(datetime.strptime(date, '%Y-%m-%d'))
+    # Get events with dates later than date
+    event_set = event_set.annotate(has_date=Max(models.Case(
+        models.When(eventdate__date__gte=query, then=1),
+        output_field=models.IntegerField(),
+    ))).filter(has_date=True)
+
+    return event_set
+
+
+def query_events_by_last_date(event_set, date):
+    """ Filters the set passed on latest event date
+
+    Parameters:
+    event_set (query_set): A query set of event objects to filter
+    date (string): date string in ISO8601 format
+
+    Returns:
+    (query_set): The filtered event set
+    """
+
+    query = timezone.make_aware(datetime.strptime(date, '%Y-%m-%d'))
+    # Get events with dates earlier than ldate
+    event_set = event_set.annotate(has_date=Max(models.Case(
+        models.When(eventdate__date__lt=query, then=1),
+        output_field=models.IntegerField(),
+    ))).filter(has_date=True)
+
+    return event_set
+
+
+def query_events_by_text_search(event_set, text_search):
+    """ Filters the set passed with by text search
+
+    Parameters:
+    event_set (query_set): A query set of event objects to filter
+    text_search (string): text string to search on
+
+    Returns:
+    (query_set): The filtered event set
+    """
+
+    queries = Q(title__icontains=text_search) | Q(description__icontains=text_search)
+    event_set = event_set.filter(queries)
+
+    return event_set
+
+
+def query_events_by_type(event_set, show_type):
+    """ Filters the set passed with by type search
+
+    Parameters:
+    event_set (query_set): A query set of event objects to filter
+    show_type (string): type name to search on
+
+    Returns:
+    (query_set): The filtered event set
+    """
+
+    zero_date = timezone.make_aware(datetime(1, 1, 1, 0, 0))
+    # Get all events matching criteria.
+    event_set = (
+        event_set.annotate(
+            first_date=Min('eventdate__date'),
+            # Last date is used to define if an event is still current
+            # or upcoming, but some event types have no dates. If there
+            # are no dates last date is set to a zero date to ensure it's
+            # dealt with as if it is in the past.
+            last_date=Coalesce(Max('eventdate__date'), zero_date),
+        ).filter(
+            type__name=show_type,
+            ).order_by('-last_date', '-first_date', '-post_date')
+        )
+
+    return event_set
+
 
 def query_events(query_list):
     """ Gets event records based on request criteria
@@ -37,7 +124,8 @@ def query_events(query_list):
             }
         }
     """
-
+    
+    now = timezone.now()
     events = Event.objects.all().order_by('-post_date')
     showcase_events = None
     event_type = None
@@ -47,53 +135,27 @@ def query_events(query_list):
         'ldate': None,
         'type': None,
     }
-    now = timezone.now()
-    zero_date = timezone.make_aware(datetime(1, 1, 1, 0, 0))
 
     # Search for dates greater than
     if 'fdate' in query_list and query_list['fdate']:
         search_query['fdate'] = query_list['fdate']
-        query = timezone.make_aware(datetime.strptime(query_list['fdate'], '%Y-%m-%d'))
-        # Get events with dates later than fdate
-        events = events.annotate(has_date=Max(models.Case(
-            models.When(eventdate__date__gte=query, then=1),
-            output_field=models.IntegerField(),
-        ))).filter(has_date=True)
+        events = query_events_by_first_date(events, query_list['fdate'])
 
     # Search for dates less than
     if 'ldate' in query_list and query_list['ldate']:
         search_query['ldate'] = query_list['ldate']
-        query = timezone.make_aware(datetime.strptime(query_list['ldate'], '%Y-%m-%d'))
-        # Get events with dates earlier than ldate
-        events = events.annotate(has_date=Max(models.Case(
-            models.When(eventdate__date__lt=query, then=1),
-            output_field=models.IntegerField(),
-        ))).filter(has_date=True)
+        events = query_events_by_last_date(events, query_list['ldate'])
 
     # Text search
     if 'q' in query_list and query_list['q']:
-        query = query_list['q']
-        search_query['text'] = query
-        queries = Q(title__icontains=query) | Q(description__icontains=query)
-        events = events.filter(queries)
+        search_query['text'] = query_list['q']
+        events = query_events_by_text_search(events, query_list['q'])
 
     # Search on event type
     if 'type' in query_list and query_list['type']:
         event_type = query_list['type']
 
-        # Get all events matching criteria.
-        events = (
-            events.annotate(
-                first_date=Min('eventdate__date'),
-                # Last date is used to define if an event is still current
-                # or upcoming, but some event types have no dates. If there
-                # are no dates last date is set to a zero date to ensure it's
-                # dealt with as if it is in the past.
-                last_date=Coalesce(Max('eventdate__date'), zero_date),
-            ).filter(
-                    type__name=event_type,
-            ).order_by('-last_date', '-first_date', '-post_date')
-        )
+        events = query_events_by_type(events, event_type)
 
         event_type = ShowType.objects.get(name=event_type)
         search_query['type'] = event_type
