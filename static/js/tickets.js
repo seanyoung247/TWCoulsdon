568ab62fc8b95a3fd52a181042fcd68b7aec635e
seanyoung247 <seanyoung247@gmail.com>
2021-05-20 01:11:59 +0100

    Feat: adds script to show and reset add tickets modal

--- static/js/tickets.js
+++ static/js/tickets.js
@@ -5,18 +5,25 @@
 // Shows the add tickets modal and requests event data from the server.
 $( '.btn-add-tickets' ).click( function() {
   const modal = $('#add-ticket-modal');
+  const url = `/boxoffice/buy_tickets/?event=${$( this ).data('event-id')}`
 
   // Set the modal title
   modal.find( '.modal-title' ).text( $( this ).data('event-title') )
 
   // Set the modal to it's preloaded state
   modal.find( '#add-tickets-form-wrapper' ).html("<h5>Loading...</h5>");
-  modal.find( '#add-tickets-ticket-list' ).html("");
+  modal.find( '#add-tickets-list' ).html("");
+  modal.find( '#add-tickets-total' ).text("0.00");
 
   // Request modal HTML from server
-  $.get( `/boxoffice/buy_tickets/?event=${$( this ).data('event-id')}`, function(data) {
+  $.get( url, function(data) {
     const formWrapper = $('#add-tickets-form-wrapper');
-    if (data.form) formWrapper.html(data.form);
+    if (data.form) {
+      formWrapper.html(data.form);
+      // Don't let the user select more tickets than there are
+      $( '#add-ticket-quantity' ).attr(
+        'max', $( '#add-ticket-date' ).find( ':selected' ).data('tickets-left'));
+    }
   });
 
   modal.modal({
