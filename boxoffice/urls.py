63950578905967ef6a0ffacba2b577c96a959b47
seanyoung247 <seanyoung247@gmail.com>
2021-05-21 15:55:45 +0100

    feat: adds initial checkout view

--- boxoffice/urls.py
+++ boxoffice/urls.py
@@ -4,9 +4,14 @@ from . import views
 
 urlpatterns = [
     #path('', views.boxoffice, name='boxoffice'),
+    # Add Ticket Dialog
     path('buy_tickets/', views.buy_tickets, name="buy_tickets"),
+    # Shopping Basket
     path('basket/', views.view_basket, name="view_basket"),
     path('basket/add/', views.add_to_basket, name="add_to_basket"),
     path('basket/remove/', views.remove_from_basket, name='remove_from_basket'),
     path('basket/update/', views.update_basket, name='update_basket'),
+    # Checkout
+    path('checkout/', views.checkout, name='checkout')
+    # Reports
 ]
