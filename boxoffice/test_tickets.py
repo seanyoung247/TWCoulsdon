71844083237b16dbdd9850ae7a6c428ea26e559c
seanyoung247 <seanyoung247@gmail.com>
2021-05-22 15:57:57 +0100

    test: adds tests for basket availability

--- boxoffice/test_tickets.py
+++ boxoffice/test_tickets.py
@@ -45,20 +45,48 @@ class TestCheckAvailability(TestCase):
         self.ticket_types = []
         self.ticket_types.append(TicketType.objects.create(
             name = "test", display_name = "Test Ticket"))
+        self.ticket_types.append(TicketType.objects.create(
+            name = "test2", display_name = "Test Ticket2"))
 
         # Create tickets for event_two
         for date in self.dates_event_two:
             for i in range(0, 5):
                 Ticket.objects.create(
-                    order = self.order, type = self.ticket_type,
+                    order = self.order, type = self.ticket_types[0],
                     event = self.event_two, date = date
                 )
 
         # Create some test baskets
+        self.good_basket = {}   # All good
+        self.bad_basket = {}    # All bad
+        self.mixed_basket = {}  # Some good, some bad
+        # Create a basket with available tickets
+        for date in self.dates_event_two:
+            self.good_basket[date.id] = {
+                self.ticket_types[0].id: 2,
+                self.ticket_types[1].id: 2,
+            }
 
+        # Create a basket with unavailable tickets
+        for date in self.dates_event_two:
+            self.bad_basket[date.id] = {
+                self.ticket_types[0].id: 4,
+                self.ticket_types[1].id: 4,
+            }
+        # Create a basket with a mix of available and unavailable tickets
+        self.mixed_basket[self.date_event_one.id] = {
+            self.ticket_types[0].id: 4,
+            self.ticket_types[1].id: 4,
+        }
+        for date in self.dates_event_two:
+            self.mixed_basket[date.id] = {
+                self.ticket_types[0].id: 4,
+                self.ticket_types[1].id: 4,
+            }
 
 
     def test_check_ticket_available(self):
+        """ Tests correct returns from check_ticket_available """
         # Check correct return if no tickets have been bought
         self.assertTrue(check_ticket_available(self.date_event_one, 2))
         # Check correct return if tickets have been bought, but enough are left
@@ -68,6 +96,21 @@ class TestCheckAvailability(TestCase):
         # Check correct return if there are not enough tickets
         self.assertFalse(check_ticket_available(self.dates_event_two[0],8))
 
+
+    def test_check_basket_availability(self):
+        """ Tests correct returns from check_basket_availability """
+        # Check correct return from a basket with availability
+        self.assertTrue(check_basket_availability(self.good_basket))
+        # Check correct return from a basket with no availability
+        self.assertRaises(Tickets_Not_Available, check_basket_availability,
+                            self.bad_basket)
+        # Check correct return from a basket with some availability
+        self.assertRaises(Tickets_Not_Available, check_basket_availability,
+                            self.mixed_basket)
+        try:
+            check_basket_availability(self.mixed_basket)
+        except Tickets_Not_Available as e:
+            self.assertEqual(e.date_id, self.dates_event_two[0].id)
 
 
 
