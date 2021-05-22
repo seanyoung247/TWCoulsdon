8a9ccb214933f11e95391a99ff3d33707cc0ed64
seanyoung247 <seanyoung247@gmail.com>
2021-05-22 15:25:16 +0100

    feat: adds function to check a single ticket line for availability

--- boxoffice/tickets.py
+++ boxoffice/tickets.py
@@ -1,14 +1,20 @@
 """ Tools for creating, updating and deleting tickets """
+from events.models import Event, EventDate
 from .models import TicketType, Ticket, Order
+from .queries import get_available_tickets_for_date
 
 
-def create_tickets():
-    """
+def check_ticket_available(event_date, quantity):
+    """ Checks that there are enough tickets left for the date passed
 
     Parameters:
+    date (EventDate): Date to check
+    quantity (int): Quantity required
 
+    Returns:
+    (boolean): True if enough tickets left
     """
-    pass
+    return (get_available_tickets_for_date(event_date) >= quantity)
 
 
 def check_tickets_available():
