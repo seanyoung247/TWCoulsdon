b6f120f63fc2a3c8eb7ecd0c8ed5809000219730
seanyoung247 <seanyoung247@gmail.com>
2021-05-22 02:38:24 +0100

    feat: sets site to always use https in production

--- TWCoulsdon/settings.py
+++ TWCoulsdon/settings.py
@@ -148,6 +148,8 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    # Forces the site to use HTTPS in production
+    SECURE_SSL_REDIRECT = True
 
 WSGI_APPLICATION = 'TWCoulsdon.wsgi.application'
 
