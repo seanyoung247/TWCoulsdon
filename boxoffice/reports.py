1ba9db45105fb5d4dfe4ed676f3f9e40e5ede6a6
seanyoung247 <seanyoung247@gmail.com>
2021-05-22 22:32:23 +0100

    feat: adds code to return ticket pdf in httpresponse

--- boxoffice/reports.py
+++ boxoffice/reports.py
@@ -1,8 +1,9 @@
-""" Generates pdf reports and tickets """
+""" Generates reports and tickets in pdf and csv formats """
 from __future__ import unicode_literals
 
 #from django.http import HttpResponse
 from django.template.loader import render_to_string
+from django.http import JsonResponse, HttpResponseBadRequest, HttpResponse
 #from django.utils.text import slugify
 #from django.conf import settings
 
@@ -14,7 +15,8 @@ from .models import TicketType, Ticket, Order
 
 
 def report_tickets_for_event(request, event):
-    """ Generates a pdf of the tickets in a given order
+    """
+    Generates a pdf of the tickets in a given order
 
     Parameters:
     request (Request): The request object
@@ -27,7 +29,8 @@ def report_tickets_for_event(request, event):
 
 
 def report_tickets_for_date(request, date):
-    """ Generates a pdf of the tickets in a given order
+    """
+    Generates a pdf of the tickets in a given order
 
     Parameters:
     request (Request): The request object
@@ -40,7 +43,8 @@ def report_tickets_for_date(request, date):
 
 
 def generate_ticket_pdf(request, order):
-    """ Generates a pdf of the tickets in a given order
+    """
+    Generates a pdf of the tickets in a given order
 
     Parameters:
     request (Request): The request object
@@ -63,5 +67,41 @@ def generate_ticket_pdf(request, order):
 
     # Generate a pdf from the HTML
     font_config = FontConfiguration()
-    return HTML(string=html, base_url=request.build_absolute_uri()
-                ).write_pdf(font_config=font_config)
+    ticket_pdf = HTML(
+        string=html,
+        base_url=request.build_absolute_uri()
+    ).write_pdf(font_config=font_config)
+
+    return ticket_pdf
+
+
+def send_ticket_pdf_email(request, order):
+    """
+    Sends a pdf for the tickets in a given order via email on the order.
+
+    Parameters:
+    request (Request): The request object
+    order (Order): The order to send tickets for
+    """
+    ticket_pdf = generate_ticket_pdf(request, order)
+    # Emailling code goes here...
+
+
+def send_ticket_pdf_http(request, order):
+    """
+    Constructs a HTTP response and embeds a pdf of the tickets
+    in a given order.
+
+    Parameters:
+    request (Request): The request object
+    order (Order): The order to send tickets for
+
+    Returns:
+    (HttpResponse)
+    """
+    ticket_pdf = generate_ticket_pdf(request, order)
+
+    response = HttpResponse(ticket_pdf, content_type='application/pdf')
+    response['Content-Disposition'] = 'inline; tickets.pdf'
+
+    return response
