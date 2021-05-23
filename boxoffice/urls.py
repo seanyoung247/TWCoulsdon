e3a4cf248dc6686d175bff5a815575d017abe28b
seanyoung247 <seanyoung247@gmail.com>
2021-05-23 14:48:20 +0100

    feat: adds webhook to urls

--- boxoffice/urls.py
+++ boxoffice/urls.py
@@ -1,6 +1,7 @@
 """ Defines the boxoffice app's routes """
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     #path('', views.boxoffice, name='boxoffice'),
@@ -15,6 +16,7 @@ urlpatterns = [
     path('checkout/', views.checkout, name='checkout'),
     path('checkout/cache_data/', views.cache_checkout_data, name='cache_checkout_data'),
     path('checkout_success/<order_number>/', views.checkout_success, name='checkout_success'),
+    path('wh/', webhook, name='webhook'),
     # Reports
     path('tickets/<order_number>/', views.get_tickets, name='get_tickets'),
 ]
