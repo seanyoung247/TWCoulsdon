2bd62d14144ff4bb55274772f8d323b88b22836d
seanyoung247 <seanyoung247@gmail.com>
2021-05-25 23:36:15 +0100

    chore: removes email field from profile

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,7 @@
+""" Defines the profile app's models """
+# pylint: disable=E5142
+# Importing user model link in user profile
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
@@ -11,7 +15,6 @@ class UserProfile(models.Model):
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
-    default_email = models.EmailField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.user.username
