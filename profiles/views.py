44ed11ecfe48e2629fccf6b0f187c501acca7c48
seanyoung247 <seanyoung247@gmail.com>
2021-05-26 00:26:10 +0100

    feat: adds post handler to profiles view

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.contrib import messages
+
+from boxoffice.models import Order
 
 from .models import UserProfile
 from .forms import UserInfoForm, UserProfileForm
@@ -13,15 +16,28 @@ def profile(request):
     # Get the current user's profile
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    if request.method == 'POST':
+        user_form = UserInfoForm(request.POST, instance=request.user)
+        profile_form = UserProfileForm(request.POST, instance=profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your profile information has been updated successfully.')
+        else:
+            messages.error(request, 'Profile update failed. Please ensure your details are valid.')
+    else:
+        # Construct the user's forms
+        user_form = UserInfoForm(instance=profile.user)
+        profile_form = UserProfileForm(instance=profile)
 
-    # Construct the user's forms
-    user_form = UserInfoForm(instance=profile.user)
-    profile_form = UserProfileForm(instance=profile)
+    # Get the user's previous orders
+    orders = profile.orders.all()
 
     context = {
         'user_form': user_form,
         'profile_form': profile_form,
-        #'orders': orders,
+        'orders': orders,
     }
 
     return render(request, 'profiles/profile.html', context)
+    
