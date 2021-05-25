a75485c7bc52546ab4d31cbed0754f307e8724ee
seanyoung247 <seanyoung247@gmail.com>
2021-05-25 23:32:39 +0100

    feat: adds help text to checkout form

--- boxoffice/forms.py
+++ boxoffice/forms.py
@@ -9,16 +9,20 @@ class OrderForm(forms.ModelForm):
     """ Defines the form for the order model """
     class Meta:
         model = Order
-        fields = ('full_name', 'email','phone_number',)
+        fields = ('full_name', 'email', 'phone_number',)
 
     def __init__(self, *args, **kwargs):
         """ Creates placeholders and classes for form. """
         super().__init__(*args, **kwargs)
 
         placeholders = {
-            'full_name': 'Full Name',
+            'full_name': 'Your Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
+        }
+        help_text = {
+            'email': "Your tickets will be sent to this address",
+            'phone_number': 'We may need to contact you on this number, for instance if a show is cancelled.',
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
@@ -28,6 +32,9 @@ class OrderForm(forms.ModelForm):
                 placeholder = f'{placeholders[field]} *'
             else:
                 placeholder = placeholders[field]
+
+            if field in help_text:
+                self.fields[field].help_text = help_text[field]
 
             self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
