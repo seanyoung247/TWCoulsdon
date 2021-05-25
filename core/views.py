4cb73b7cac42ae3d1af643b5b3a995e421e8ea0d
seanyoung247 <seanyoung247@gmail.com>
2021-05-25 18:56:05 +0100

    feat: adds 404 view

--- core/views.py
+++ core/views.py
@@ -5,3 +5,7 @@ from django.conf import settings
 def redirect_ico(request):
     """ Redirects any errant requests for the favicon to it's true location """
     return redirect(settings.FAVICON)
+
+
+def page_not_found_view(request, exception=None):
+    return render(request, 'errors/404.html')
