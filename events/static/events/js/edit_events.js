9d32f290b00e187123a7998043a613486be46bdd
seanyoung247 <seanyoung247@gmail.com>
2021-05-27 00:48:40 +0100

    feat: adds code to add gallery images

--- events/static/events/js/edit_events.js
+++ events/static/events/js/edit_events.js
@@ -10,10 +10,51 @@ tinymce.init({
 });
 
 // Sets the preview when a file is selected
-$( '#add-image-chooser' ).change(function() {
-  const imagePreview = $( this ).siblings("#add-image-preview");
+$( '#add_gallery_list' ).on('change', '.image-chooser', function() {
+  const imagePreview = $( this ).siblings(".upload-image-preview");
   if (this.files && this.files[0]) {
     const src = URL.createObjectURL(this.files[0]);
     imagePreview.attr('src', src);
   }
+});
+
+// Clears the error hint on changing the required image fields
+$( '#add-image-chooser' ).change(function() {
+  $( this ).removeClass('add-image-invalid');
+});
+$( '#add-image-name' ).change(function() {
+  $( this ).removeClass('add-image-invalid');
+});
+
+// Adds another image to the list
+$( '#add-image-btn' ).click(function() {
+  // Create the html for the new image item
+  html = `
+  <li class="upload-image-item">
+    <div class="row">
+      <label>
+        <img class="upload-image-preview" src="${$(this).data('no-image')}">
+        <input name="existing-image" type="hidden" value="none">
+        <input class="image-chooser hidden-image-input" name="new-image" type="file"
+                accept="image/png, image/jpeg" required>
+      </label>
+      <div class="col text-right">
+        <input name="image-name" class="form-control mb-1" type="text" placeholder="Image name"
+                value="" required>
+        <input name="image-description" class="form-control mb-1"
+                type="text" placeholder="Image description"
+                value="">
+        <button class="remove-image-btn btn btn-twc-brand" type="button">
+          Remove
+        </button>
+      </div>
+    </div>
+  </li>`
+  // Create the new element and add it to the list
+  $( '#add_gallery_list' ).prepend(html)
+});
+
+// Remove an image from the gallery
+$( '#add_gallery_list' ).on( 'click', '.remove-image-btn', function() {
+  $( this ).closest( 'li.upload-image-item' ).remove();
 });
