3fffe971178993bda4219599c538569d8a46bb2d
seanyoung247 <seanyoung247@gmail.com>
2021-05-26 22:21:55 +0100

    feat: adds code to initialise rich text editor

---
+++ events/static/events/js/edit_events.js
@@ -0,0 +1,10 @@
+/*
+ * Provides code for the edit events page
+ */
+
+ //Initialises the rich text input field
+tinymce.init({
+  selector: '#id_description',
+  plugins: 'advlist autolink lists link image charmap preview hr anchor',
+  toolbar_mode: 'floating',
+});
