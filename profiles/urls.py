3532317357ad93b0a43c608120352f7b83d71f44
seanyoung247 <seanyoung247@gmail.com>
2021-05-26 03:15:54 +0100

    feat: adds order history url

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
+    path('order_history/<order_number>', views.order_history, name='order_history')
+    #path('load_order_page/<page>', views.load_order_page, name='load_order_page'),
 ]
