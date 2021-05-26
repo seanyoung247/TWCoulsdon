8bc9c8bca9210acb78ac40bfb3204bf018324249
seanyoung247 <seanyoung247@gmail.com>
2021-05-26 12:50:44 +0100

    feat: adds initial image form

--- events/forms.py
+++ events/forms.py
@@ -1,7 +1,7 @@
 """ Defines the forms used by the events app """
 
 from django import forms
-from .models import ShowType, EventDate, Event, Venue, Image
+from .models import EventDate, Event, Venue, Image
 
 
 class EventDateForm(forms.modelForm):
@@ -17,3 +17,28 @@ class EventDateForm(forms.modelForm):
 
         self.fields['date'].widget.attrs['placeholder'] = 'Date'
         self.fields[field].label = False
+
+
+class ImageForm(forms.modelForm):
+    """ Creates a form to create and update images """
+    class Meta:
+        model = Image
+        exclude = ('event',)
+
+    def __init__(self, *args, **kwargs):
+        """ Initialises the EventDate form """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'name': 'Name',
+            'description': 'Description',
+            'image': 'Image',
+        }
+
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].label = False
