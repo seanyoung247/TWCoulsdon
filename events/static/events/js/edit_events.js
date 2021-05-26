744ebff452adb2eb0141288122a529b36439b571
seanyoung247 <seanyoung247@gmail.com>
2021-05-26 22:22:18 +0100

    feat: adds code to preview images selected for upload

--- events/static/events/js/edit_events.js
+++ events/static/events/js/edit_events.js
@@ -8,3 +8,12 @@ tinymce.init({
   plugins: 'advlist autolink lists link image charmap preview hr anchor',
   toolbar_mode: 'floating',
 });
+
+// Sets the preview when a file is selected
+$( '#add-image-chooser' ).change(function() {
+  const imagePreview = $( this ).siblings("#add-image-preview");
+  if (this.files && this.files[0]) {
+    const src = URL.createObjectURL(this.files[0]);
+    imagePreview.attr('src', src);
+  }
+});
