8eb313b656b66bd72af345faa69ca073850c2a3c
seanyoung247 <seanyoung247@gmail.com>
2021-05-27 20:57:06 +0100

    feat: adds code to set admin tools on image show

--- events/static/events/js/edit_gallery.js
+++ events/static/events/js/edit_gallery.js
@@ -10,3 +10,22 @@ $( document ).ready(function() {
   addImageItem.insertBefore('.scroll-item-bookend:last');
   addImageItem.removeClass('d-none');
 });
+
+// Hook into the gallery-image event so we can set admin
+// tools when the gallery is shown
+$( '.gallery-image-link' ).click(function(){
+  const current = $( this );
+  const currentData = current.data('description');
+  const titleInput = $( '#gallery-admin-title' );
+  const addImageBtn = $( '#gallery-add-image-btn' );
+  // Set the contents of the image title input
+  titleInput.val(currentData);
+  // Is this the add image button
+  if (currentData == 'Add new image') {
+    // Show the add image button
+    addImageBtn.show();
+  } else {
+    // Hide the add image button
+    addImageBtn.hide();
+  }
+});
