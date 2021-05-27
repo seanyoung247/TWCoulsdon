9627d14eb60e3ae50f411bcbcc2793782ab8b13c
seanyoung247 <seanyoung247@gmail.com>
2021-05-27 11:16:32 +0100

    feat: adds code to create post request

--- events/static/events/js/edit_events.js
+++ events/static/events/js/edit_events.js
@@ -89,6 +89,62 @@ $( '#add-image-btn' ).click(function(e) {
 });
 
 // Remove an image from the gallery
-$( '#add_gallery_list' ).on( 'click', '.remove-image-btn', function() {
+$( '#add-gallery-list' ).on( 'click', '.remove-image-btn', function(e) {
+  e.preventDefault();
   $( this ).closest( 'li.upload-image-item' ).remove();
+});
+
+// Captures the submit event so we can set the title image
+const form = $( '#event-form' );
+form[0].addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  const csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+  let dates = [];
+  let images = [];
+
+  // Construct the list of event dates
+  $( '#add-date-list' ).children('li').each(function(){
+    const date = $( this ).children('input[name="event_date"]');
+    const time = $( this ).children('input[name="event_time"]');
+    dates.push({
+      'date': $.trim(date.val()),
+      'time': $.trim(time.val())
+    });
+  });
+  // Construct the list of gallery images
+  $( '#add-gallery-list' ).children('li').each(function() {
+    images.push({
+      'existing_image': $.trim($( this ).children('input[name="existing_image"]')),
+      'new_image': $.trim($( this ).children('input[name="new_image"]')),
+      'image_name': $.trim($( this ).children('input[name="image_name"]')),
+      'image_description': $.trim($( this ).children('input[name="image_description"]')),
+      'title_image': $.trim($( this ).children('input[name="title_image"]'))
+    });
+  });
+
+  url = form.attr('action');
+  // Construct the data to submit
+  postData = {
+    'csrfmiddlewaretoken': csrfToken,
+    'title': $.trim(form[0].title.value),
+    'author': $.trim(form[0].author.value),
+    'tagline': $.trim(form[0].tagline.value),
+    'description': $.trim(form[0].description.value),
+    'type': $.trim(form[0].type.value),
+    'venue': $.trim(form[0].venue.value),
+    'content': $.trim(form[0].content.value),
+    'dates': dates,
+    'images': images
+  }
+  const event_id = $( 'input[name="event_id"' ).val();
+
+  console.log(postData);
+  if (event_id) postData['event_id'] = event_id;
+
+  $.post(url, postData).done(function(data) {
+  //  console.log(data);
+  });
+
+
 });
