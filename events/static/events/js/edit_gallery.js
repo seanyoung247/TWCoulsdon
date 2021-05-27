5dd1a20d62f23b7a1dbdc547f853336a92e1f6d3
seanyoung247 <seanyoung247@gmail.com>
2021-05-27 21:42:59 +0100

    feat: adds code to setup admin tools for adding images

--- events/static/events/js/edit_gallery.js
+++ events/static/events/js/edit_gallery.js
@@ -18,15 +18,25 @@ $( '.gallery-image-link' ).click(function(){
   const currentData = current.data('description');
   const titleInput = $( '#gallery-admin-title' );
   const addImageBtn = $( '#gallery-add-image-btn' );
+  const updateImageBtn = $( '#gallery-admin-update' );
+  const deleteImageBtn = $( '#gallery-admin-delete' );
   // Set the contents of the image title input
   titleInput.val(currentData);
+  // Clear the file upload, don't stash selected files
+  $( '#image-upload' ).val('');
   // Is this the add image button
   if (currentData == 'Add new image') {
     // Show the add image button
     addImageBtn.show();
+    // Update button adds images here:
+    updateImageBtn.text("Add");
+    // Delete button clears the selected file
+    deleteImageBtn.text('Cancel');
   } else {
     // Hide the add image button
     addImageBtn.hide();
+    updateImageBtn.text('Update');
+    deleteImageBtn.text('Delete');
   }
 });
 
