3fe66453894d26581c95b6a27e144fd88c4598c0
seanyoung247 <seanyoung247@gmail.com>
2021-05-27 13:30:54 +0100

    chores: removes unneeded code from event.forms

--- events/forms.py
+++ events/forms.py
@@ -11,46 +11,14 @@ class EventDateForm(forms.ModelForm):
     """ Creates a form to create and update event dates """
     class Meta:
         model = EventDate
-        # Event is generated automatically on creation
-        exclude = ('event',)
-        widgets = {
-            'date': forms.SplitDateTimeWidget(
-                    attrs={'class': 'shite'},
-                    date_attrs={'type': 'date'},
-                    time_attrs={'type': 'time'},
-                )
-        }
-
-    def __init__(self, *args, **kwargs):
-        """ Initialises the EventDate form """
-        super().__init__(*args, **kwargs)
-
-        self.fields['date'].label = False
+        fields = ('event', 'date',)
 
 
 class ImageForm(forms.ModelForm):
     """ Creates a form to create and update images """
     class Meta:
         model = Image
-        exclude = ('event',)
-
-    def __init__(self, *args, **kwargs):
-        """ Initialises the EventDate form """
-        super().__init__(*args, **kwargs)
-        placeholders = {
-            'name': 'Name',
-            'description': 'Description',
-            'image': 'Image',
-        }
-
-        for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-
-            self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].label = False
+        fields = ('event', 'name', 'description', 'image',)
 
 
 class ShowTypeModelChoiceField(forms.ModelChoiceField):
