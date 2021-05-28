7359ae73f92c6f15dac0b287c24044bc0a27adf4
seanyoung247 <seanyoung247@gmail.com>
2021-05-28 19:33:43 +0100

    feat: adds delete event path

--- events/urls.py
+++ events/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('lazy_load/', views.lazy_load_events, name='lazy_load_events'),
     path('venue/<venue_id>', views.venue_details, name='venue_details'),
     path('edit_event/', views.edit_event, name='edit_event'),
+    path('edit_event/delete_event/', views.delete_event, name='delete_event'),
     path('edit_event/remove_date/', views.remove_date, name='remove_date'),
     path('edit_event/add_image/', views.add_image, name='add_image'),
     path('edit_event/edit_image/', views.edit_image, name='edit_image'),
