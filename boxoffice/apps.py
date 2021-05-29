44e0a0d03fb2a1a8c95a93d7e5e819b452183312
seanyoung247 <seanyoung247@gmail.com>
2021-05-29 02:02:50 +0100

    fix: fixes linting warnings in boxoffice.apps

--- boxoffice/apps.py
+++ boxoffice/apps.py
@@ -1,7 +1,9 @@
+""" App configuration """
 from django.apps import AppConfig
 
 
 class BoxofficeConfig(AppConfig):
+    """ App configuration """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'boxoffice'
 
